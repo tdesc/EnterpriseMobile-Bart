0a7514b42e5a78584179b629d0b0bb8d442b377b
Bart Kummel <bart@kummelweb.nl>
2013-06-12 21:20:43 +0200

    Vlaggen toegevoegd

--- EnterpriseMobileMeteor.js
+++ EnterpriseMobileMeteor.js
@@ -55,39 +55,26 @@ if (Meteor.isClient) {
     }) 
   };
   
-  Handlebars.registerHelper('selected', function(foo, bar) {
+  Template.edit_wijn.selected = function(foo, bar) {
     return foo == bar ? 'selected' : '';
-  });
+  };
   
   Template.wijn.selected = function () {
     return Session.equals("selected_wijn", this._id) ? "selected" : '';
   };
+  
+  Template.wijn.landcode = function(landNaam) {
+    land = Landen.findOne({naam: landNaam});
+    if (land) {
+      return land.code
+    } else {
+      return ""
+    }
+  }
 
   Template.wijn.events({
     'click': function () {
       Session.set("selected_wijn", this._id);
     }
   });
-}
-
-if (Meteor.isServer) {
-    Meteor.startup(function () {
-     if (Wijnen.find().count() === 0) {
-        var wijnen = [
-          new wijn("Naam1", "Appelatie1", "Streek1", "Land1", "Druif1", "rood", "Soort1"),
-          new wijn("Naam2", "Appelatie2", "Streek2", "Land2", "Druif2", "rood", "Soort2"),
-          new wijn("Naam3", "Appelatie3", "Streek3", "Land3", "Druif3", "wit", "Soort3"),
-          new wijn("Naam4", "Appelatie4", "Streek4", "Land4", "Druif4", "wit", "Soort4"),
-          new wijn("Naam5", "Appelatie5", "Streek5", "Land5", "Druif5", "rosé", "Soort5")];
-        for (var i = 0; i < wijnen.length; i++)
-          Wijnen.insert({naam: wijnen[i].naam,
-                         appellatie: wijnen[i].appellatie,
-                         streek: wijnen[i].streek,
-                         land: wijnen[i].land,
-                         druif: wijnen[i].druif,
-                         kleur: wijnen[i].kleur,
-                         soort: wijnen[i].soort});
-      }
-    });
-
 }
