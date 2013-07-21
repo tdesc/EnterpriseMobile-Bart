705c509359a8d9e1cb9c12e1d739591750aec277
Bart Kummel <bart@kummelweb.nl>
2013-07-21 13:37:31 +0200

    Added select2 to select multiple aromas

--- client/editSmaaknotitie.js
+++ client/editSmaaknotitie.js
@@ -1,3 +1,30 @@
+Template.edit_smaaknotitie.rendered = function() {
+   // TODO: why is normal binding not working here?
+   console.log(Session.get("notitie").geur)
+   $('#geur').val(Session.get("notitie").geur)
+   console.log($("#geur").val())
+   $("#geur").select2({
+     multiple: true,
+     query: function(query) {
+       var aromas = Aromas.find({aroma: {$regex: query.term}});
+       var results = aromas.map(function(aroma){
+         return {id: aroma.nummer, text: aroma.aroma}
+       }); 
+       query.callback({results: results});
+     },
+     initSelection: function(element, callback) {
+       var data = [];
+       _.each(element.val().split(","), function (nummer) {
+         var aroma = Aromas.findOne({nummer: nummer})
+         if (aroma != null) {
+           data.push({id: nummer, text: aroma.aroma});
+         }
+       });
+       callback(data);
+     }
+   });
+}
+
 Template.edit_smaaknotitie.events({
   'click button.save' : function() {
     var notitie = Template.edit_smaaknotitie.notitie();
